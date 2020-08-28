1cf500f5c927515e9f6c66afe0d9fa8600a44a38
Joe Snell <joe.snell@daveramsey.com>
2020-08-28 10:58:18 -0500

    update

--- .ramsey/cicd/app.ts
+++ .ramsey/cicd/app.ts
@@ -1,49 +1,69 @@
 import rs from "rs-cdk";
-import hub from 'rs-cdk/accounts/hub';
-import * as codebuild from '@aws-cdk/aws-codebuild';
-import * as iam from '@aws-cdk/aws-iam';
-import * as ecr from '@aws-cdk/aws-ecr';
+import hub from "rs-cdk/accounts/hub";
+import * as codebuild from "@aws-cdk/aws-codebuild";
+import * as iam from "@aws-cdk/aws-iam";
+import * as ecr from "@aws-cdk/aws-ecr";
 
 const app = new rs.core.App({
-    billing: rs.core.BillingTags.GLOBAL,
-    name: 'salesforce-apex-mocks'
+  billing: rs.core.BillingTags.GLOBAL,
+  name: "salesforce-apex-mocks",
 });
 
 const stack = new rs.core.Stack(app, `${app.repo.name}-cicd`, {
-  env: hub.cicd
+  env: hub.cicd,
 });
 
 // this will eventually move
-const role = new iam.Role(stack, 'BuildRole', {
-    assumedBy: new iam.ServicePrincipal('codebuild.amazonaws.com')
+const role = new iam.Role(stack, "BuildRole", {
+  assumedBy: new iam.ServicePrincipal("codebuild.amazonaws.com"),
 });
 
-role.addToPolicy(new iam.PolicyStatement({
+role.addToPolicy(
+  new iam.PolicyStatement({
     actions: [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents",
-        "codebuild:CreateReportGroup",
-        "codebuild:CreateReport",
-        "codebuild:UpdateReport",
-        "codebuild:BatchPutTestCases"
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "codebuild:CreateReportGroup",
+      "codebuild:CreateReport",
+      "codebuild:UpdateReport",
+      "codebuild:BatchPutTestCases",
     ],
-    resources: ['*']
-}));
+    resources: ["*"],
+  })
+);
 
+role.addToPolicy(
+  new iam.PolicyStatement({
+    actions: ["ssm:GetParameter*"],
+    resources: [
+      `arn:aws:ssm:${stack.region}:${hub.cicd.account}:parameter/salesforce/*`,
+    ],
+  })
+);
 
-role.addToPolicy(new iam.PolicyStatement({
+role.addToPolicy(
+  new iam.PolicyStatement({
     actions: [
-        "ssm:GetParameter*"
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:BatchGetImage",
+      "ecr:GetDownloadUrlForLayer",
     ],
     resources: [
-        `arn:aws:ssm:us-east-1:${hub.cicd.account}:parameter/salesforce/*`    ]
-}))
+      `arn:aws:ecr:${stack.region}:${hub.cicd.account}:repository/crm-appirio-base:*`,
+    ],
+  })
+);
 
 new rs.cicd.PRBuild(stack, "PRBuild", {
-    repo: app.repo,
-    role,
-    buildImage: codebuild.LinuxBuildImage.fromEcrRepository(
-        ecr.Repository.fromRepositoryName(stack, `AppirioBaseRepo`, 'crm-appirio-base:7')
-    )
+  repo: app.repo,
+  role,
+  buildImage: codebuild.LinuxBuildImage.fromEcrRepository(
+    ecr.Repository.fromRepositoryName(
+      stack,
+      `AppirioBaseRepo`,
+      "crm-appirio-base"
+    ),
+    "7"
+  ),
 });
