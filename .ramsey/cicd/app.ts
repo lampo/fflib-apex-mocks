e70a2aa23596610e61695c9a505654581cfd6ab3
Joe Snell <joe.snell@daveramsey.com>
2020-08-28 09:16:11 -0500

    use appirio image

--- .ramsey/cicd/app.ts
+++ .ramsey/cicd/app.ts
@@ -2,6 +2,7 @@ import rs from "rs-cdk";
 import hub from 'rs-cdk/accounts/hub';
 import * as codebuild from '@aws-cdk/aws-codebuild';
 import * as iam from '@aws-cdk/aws-iam';
+import * as secrets from '@aws-cdk/aws-secretsmanager';
 
 const app = new rs.core.App({
     billing: rs.core.BillingTags.GLOBAL,
@@ -43,5 +44,7 @@ role.addToPolicy(new iam.PolicyStatement({
 new rs.cicd.PRBuild(stack, "PRBuild", {
     repo: app.repo,
     role,
-    buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2_3
+    buildImage: codebuild.LinuxBuildImage.fromDockerRegistry('appirio/dx:3.0.1.191186', {
+        secretsManagerCredentials: secrets.Secret.fromSecretArn(stack, `DockerCredentials`, 'arn:aws:secretsmanager:us-east-1:058238361356:secret:codebuild/docker-hub-credentials-xZIbrm')
+    })
 });
